8ba3b20b5a73469b80cad6966c70293f5dd75dbd
EWeezy389 <81266095+EWeezy389@users.noreply.github.com>
2021-05-19 18:11:12 -0400

    Presentational and Container Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -4,7 +4,6 @@ class CampsiteInfo extends Component {
   renderCampsite(campsite) {
     return (
       <div className="col-md-5 m-1">
-        
         <Card>
           <CardImg top src={campsite.image} alt={campsite.name} />
           <CardBody>
@@ -45,11 +44,11 @@ class CampsiteInfo extends Component {
   render() {
     if (this.props.campsite) {
       return (
-        <div className="row">
-          {this.renderCampsite(this.props.campsite)}
-          {}
-          {this.renderComments(this.props.campsite.comments)}
-          {}
+        <div className="container">
+              <div className="row">
+                  {this.renderCampsite(this.props.campsite)}
+                  {this.renderComments(this.props.campsite.comments)}
+              </div>
         </div>
       );
     }
